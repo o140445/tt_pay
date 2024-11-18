5c46ce5221a6f33e2ad6a7c8cc1811ccbf50429f
o140445 <o140445s@gmail.com>
2024-11-18 21:47:05 +0800

    googel验证

--- public/assets/js/manystore/security.js
+++ public/assets/js/manystore/security.js
@@ -1,10 +1,16 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'upload'], function ($, undefined, Backend, Table, Form, Upload) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'upload'], function ($, undefined, Backend, Table, Form) {
 
     var Controller = {
         index: function () {
-            Form.api.bindevent($("#shop-form"));
+            Form.api.bindevent($("#add-form"), function (data, ret) {
+                if (ret.code === 1) {
+                    // 关闭tab页
+                    // 手动关闭当前 Tab 页
+                    parent.window.location.reload();
+                }
+            });
         },
-        bindgoogle: function () {
+        bindGoogle: function () {
             Controller.api.bindevent();
         },
     };
