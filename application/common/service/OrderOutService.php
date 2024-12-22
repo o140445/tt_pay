1fa69b3d4e4b9b12a497dfd0a88effa11afedcfe
o140445 <o140445s@gmail.com>
2024-12-23 02:16:37 +0800

    Ac凭证

--- application/common/service/OrderOutService.php
+++ application/common/service/OrderOutService.php
@@ -691,7 +691,7 @@ class OrderOutService
 
         // 解析数据
         $paymentService = new PaymentService($order->channel->code);
-        $voucher = $paymentService->parseVoucher($order->channe, $data);
+        $voucher = $paymentService->parseVoucher($order->channel, $data);
 
         $extra = json_decode($order->extra, true);
 
