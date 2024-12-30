56c458c7142c78857ad2de42f578f28c9b4b9bd5
o140445 <o140445s@gmail.com>
2024-12-30 23:15:32 +0800

    代付提交限制

--- application/command/OutOrderBigRequestChannel.php
+++ application/command/OutOrderBigRequestChannel.php
@@ -30,7 +30,7 @@ class OutOrderBigRequestChannel extends Command
         // 获取所有未处理的订单 100条
         $orderOut = OrderOut::where('status', OrderOut::STATUS_UNPAID)
             ->whereIn('member_id', $big_customer_id)
-            ->limit(30)
+            ->limit(80)
             ->select();
 
         if (!$orderOut) {
